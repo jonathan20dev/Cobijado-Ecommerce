3919c47f105f9681751a0dddf5393acc803e019b
jonathan20dev <2019347969@estudiantec.cr>
2022-05-19 15:25:02 -0600

    loader

--- src/components/Public/Security/ProtectedRoute.jsx
+++ src/components/Public/Security/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../../negocio/context/AuthContext";
+import { Loading } from "./Loading"
 
 export function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <h1>Loading</h1>;
+  if (loading) return <Loading/>;
 
   if (!user) return <Navigate to="/login" />;
 
