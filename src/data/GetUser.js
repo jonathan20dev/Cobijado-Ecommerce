e45c50e5524b284bfd99afc5146beba08fadd8d5
jonathan20dev <2019347969@estudiantec.cr>
2022-05-18 21:56:32 -0600

    getUser

---
+++ src/data/GetUser.js
@@ -0,0 +1,11 @@
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+
+const getUser = async (id) => {
+    const coleccionC = collection(db, "Usuarios");
+    const coleccionSP = await getDocs(coleccionC);
+    return coleccionSP.docs.map((element) => ({ ...element.data(),  id: element.id })).find(element => element.id === id)
+};
+
+export { getUser };
